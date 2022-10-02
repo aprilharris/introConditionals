e7bde10f48ccf8c044f514ee5a37a5ae73ad88f2
April Harris <54374224+aprilharris@users.noreply.github.com>
2022-10-02 19:21:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+//INTRO TO CONDITIONALS & COMPARISON OPERATORS
+
+//IF ELSE STATEMENT
+// let grade;
+
+// grade=30;
+
+// if (grade >=65) {
+//   console.log("You passed!");
+// } else {
+//   console.log ("You failed.");
+// }
+
+//ELSE IF STATEMENT
+// let sodaSize;
+
+// sodaSize = 13;
+// sodaSize = 100;
+                                                          
+// if (sodaSize > 24) {
+//   console.log("You ordered an extra large.")
+// } else if (sodaSize >= 20) {
+//   console.log("You ordered a large.")
+// } else if (sodaSize >=14){
+//   console.log("You ordered a medium.")
+// } else{
+//   console.log("You ordered a small.")
+// };
+
+
+
+// CHAINING CONDITIONS WITH && OPERATOR
+//Write a program that determines if the user passed the course
+//The user must pass all 3 exams
+//Note that 65 is passing
+
+// let exam1=50;
+// let exam2=20;
+// let exam3=69;
+
+// if  ( (exam1 >= 65) && (exam2 >= 65) && (exam3 >= 65) ) {
+//   console.log("You PASSED the course.");
+// } else {
+//   console.log("You FAILED the course.");
+// };
+
+//CHAINING CONDITIONS WITH || OPERATOR
+//write a program that determines if user passed the course
+//they fail if they're absent more than 3 times or if they score less than 65 on the final exam
+
+// let number_of_absences = 100;
+// let finalExam = 90;
+
+
+// if ( (number_of_absences >=3) || (finalExam<65) ) {
+//   console.log("You failed the course.");
+// } else {
+//   console.log("You passed the course.");
+// }
+
+let passed_level_1;
+let passed_level_2;
+
+
+
